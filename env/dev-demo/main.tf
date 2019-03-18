f83ae3b5694ea8e9bc5f66abfbaeaa715ae10ec3
Jungyoul Yu <jungyoul.yu@bespinglobal.com>
2019-03-18 11:41:56 +0900

    snapshot_bucket_enabled

--- env/dev-demo/main.tf
+++ env/dev-demo/main.tf
@@ -26,17 +26,21 @@ module "elasticsearch" {
   instance_count = 1
   instance_type  = "t2.small.elasticsearch"
 
-  # dedicated_master_enabled = "true"
-  # dedicated_master_count = 3
-  # dedicated_master_type = "m4.large.elasticsearch"
+  dedicated_master_enabled = "true"
+  dedicated_master_count   = 3
+  dedicated_master_type    = "m4.large.elasticsearch"
 
-  ebs_volume_size         = 10
+  ebs_volume_size = 64
+
   zone_awareness_enabled  = "false"
   encrypt_at_rest_enabled = "false"
   domain_policy_enabled   = "true"
+  snapshot_bucket_enabled = "true"
+
   allow_ip_address = [
     "58.151.93.9/32", // 강남
   ]
+
   advanced_options {
     "rest.action.multi.allow_explicit_index" = "true"
   }
